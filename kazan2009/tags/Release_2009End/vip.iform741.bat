b66004cf5633a8c9516168e1e2955fb4427c7433
gorbunov.alexey.v@gmail.com <gorbunov.alexey.v@gmail.com@c108ffd6-b957-11de-b528-055f0b42e881>
2010-01-15 08:19:02 +0000

    Релиз конца 2009 года

---
+++ kazan2009/tags/Release_2009End/vip.iform741.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set timever=901234
+
+call sets.bat
+
+if exist %ApiOutFile% del %ApiOutFile%
+if exist AtlantIs.res del AtlantIs.res
+
+rem set ApiExePath=D:\GALAKTIKA\GAL810\exe
+
+rem set tempDateStr1=%DATE%
+rem date 01.01.2007
+%ApiExePath%\vip.exe iform741.prj %1
+rem date %tempDateStr1%
+
+if exist AtlantIs.res del AtlantIs.res
+
+copy /Y *.res ..\
+
+del *.res
+
+rem copy debug.res .\.tmp\gal_GorbunovAV\atlantis_gal_GorbunovAV.res
+rem copy debug.res resourses\szmn_form741.res
+
